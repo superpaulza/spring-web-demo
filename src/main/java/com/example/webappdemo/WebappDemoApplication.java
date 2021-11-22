79c906caf06e6880590954426b1acadfaae7ff1d
superpaulza <18383834+superpaulza@users.noreply.github.com>
2021-11-23 00:13:00 +0700

    init project

---
+++ src/main/java/com/example/webappdemo/WebappDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.webappdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebappDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappDemoApplication.class, args);
+	}
+
+}
